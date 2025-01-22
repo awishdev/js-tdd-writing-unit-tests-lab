fe0ddf4413d791c9afba80742933389277e24937
Arthur Ish <arthurishdev@gmail.com>
2025-01-21 22:30:45 -0800

    testing

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,38 @@
 // Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", () => {
+    it("returns true if the word is the same reversed", () => {
+
+        const word = "racecar";
+        const result = isPalindrome(word);
+
+        expect(result).toBe(true);
+    });
+    it("returns false if the word is not the same reversed", () => {
+
+        const word = "car";
+        const result = isPalindrome(word);
+
+        expect(result).toBe(false);
+    });
+    it("return true if words have uppercase and lowercase", () => {
+
+        const word = "TacOCat";
+        const result = isPalindrome(word);
+
+        expect(result).toBe(true);
+    });
+    it("throw an error if there any non-alphabet characters", () => {
+
+        const word = "t4co c4t";
+        
+        expect(() => isPalindrome(word)).toThrow();
+    });
+    it("throws an error if its an empty string", () => {
+
+        const word = "";
+
+        expect(() => isPalindrome(word)).toThrow();
+    });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,16 @@
 // Your code here
+
+export function isPalindrome(word){
+
+
+    if (!/^[A-Za-z]+$/.test(word)) {
+
+        throw new Error("Only Alhabet Letters");
+
+    }
+    
+    const wordLowerCase = word.toLowerCase();
+    const wordBackwards = wordLowerCase.split("").reverse().join("");
+
+    return wordLowerCase === wordBackwards;
+}
